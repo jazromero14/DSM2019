7a0e68037cac18f3c1733022c8462e0629135602
Kevin Ariel Hernández Martínez <43228001+Kaiwen20111998@users.noreply.github.com>
2020-02-03 16:15:31 -0600

    Formulario falta
    Agrega todo para el formulario y yo lo recibo

--- Ejemplos/app/src/main/java/sv/edu/udb/ejemplos/Dashboard.kt
+++ Ejemplos/app/src/main/java/sv/edu/udb/ejemplos/Dashboard.kt
@@ -1,12 +1,20 @@
 package sv.edu.udb.ejemplos
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class Dashboard : AppCompatActivity() {
+
+    private lateinit var usernameText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val extras = intent.extras
+        if (!extras!!.isEmpty) {
+            usernameText.text = extras.getString("Usuario", "Usuario Invitado")
+        }
     }
 }
